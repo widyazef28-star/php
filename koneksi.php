4068f012b659214c962a72db7cd89d5bfbfdea12
widyazef28-star <widyazef28@gmail.com>
2026-05-07 15:05:32 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,7 @@
 // kalau tidak ada baru pakai data di dalam kutip.
 $host = getenv('MYSQLHOST') ?: "mysql.railway.internal";
 $user = getenv('MYSQLUSER') ?: "root";
-$pass = getenv('MYSQLPASSWORD') ?: "LZstNChgUtUIzpLIpzjVtJVusCMZscPX";
+$pass = getenv('MYSQLPASSWORD') ?: "XjoAlLUEkOLWBxVdyakegkQDsTDsZkAZ";
 $db   = getenv('MYSQLDATABASE') ?: "railway";
 $port = getenv('MYSQLPORT') ?: 3306;
 
